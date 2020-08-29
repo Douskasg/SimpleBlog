9a1db7c8ad4d235d2513c3bfdee10182bcc066a8
Douskasg <douskasg@gmail.com>
2020-08-29 20:35:21 +0300

    Added post admin index

---
+++ SimpleBlog/Areas/Admin/ViewModels/Posts.cs
@@ -0,0 +1,13 @@
+﻿using SimpleBlog.Infrastructure;
+using SimpleBlog.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SimpleBlog.Areas.Admin.ViewModels
+{
+    public class PostsIndex
+    {
+        public PagedData<Post> Posts { get; set; }
+    }
+}
--- SimpleBlog/Models/Post.cs
+++ SimpleBlog/Models/Post.cs
@@ -17,8 +17,8 @@ namespace SimpleBlog.Models
         public virtual string Content { get; set; }
 
         public virtual DateTime CreatedAt { get; set; }
-        public virtual DateTime UpdatedAt { get; set; }
-        public virtual DateTime DeletedAt { get; set; }
+        public virtual DateTime? UpdatedAt { get; set; }
+        public virtual DateTime? DeletedAt { get; set; }
 
         public virtual IList<Tag> Tags { get; set; }
 
