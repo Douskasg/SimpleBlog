71fd6c1de721a459efcbb7e1b603b410282da439
Douskasg <douskasg@gmail.com>
2020-09-01 18:13:24 +0300

    Created new and edit actions for post controller

--- SimpleBlog/Areas/Admin/Controllers/PostsController.cs
+++ SimpleBlog/Areas/Admin/Controllers/PostsController.cs
@@ -6,6 +6,7 @@ using System.Web;
 using System.Web.Mvc;
 using SimpleBlog.Models;
 using SimpleBlog.Areas.Admin.ViewModels;
+using System.EnterpriseServices;
 
 namespace SimpleBlog.Areas.Admin.Controllers
 { 
@@ -29,6 +30,66 @@ namespace SimpleBlog.Areas.Admin.Controllers
             {
                 Posts = new PagedData<Post>(currentPostPage, totalPostCount, page, PostsPerPage)
             });
+        }
+
+        public ActionResult New()
+        {
+            return View("Form", new PostsForm
+            {
+                IsNew = true
+            });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var post = Database.Session.Load<Post>(id);
+            if (post == null)
+                return HttpNotFound();
+
+            return View("Form", new PostsForm
+            {
+                IsNew = false,
+                PostId = id,
+                Content = post.Content,
+                Slug = post.Slug,
+                Title = post.Title
+            });
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult Form(PostsForm form)
+        {
+            form.IsNew = form.PostId == 0;
+
+            if (!ModelState.IsValid)
+                return View(form);
+
+            Post post;
+            if (form.IsNew)
+            {
+                post = new Post
+                {
+                    CreatedAt = DateTime.UtcNow,
+                    User = Auth.User
+                };
+            }
+            else
+            {
+                post = Database.Session.Load<Post>(form.PostId);
+
+                if (post == null)
+                    return HttpNotFound();
+
+                post.UpdatedAt  = DateTime.UtcNow;
+            }
+
+            post.Title = form.Title;
+            post.Slug = form.Slug;
+            post.Content = form.Content;
+
+            Database.Session.SaveOrUpdate(post);
+
+            return RedirectToAction("Index");
         }
     }
 }
