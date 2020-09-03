a2bc7b8e32f3202c90c1d135a1a7054e88cbf62e
Douskasg <douskasg@gmail.com>
2020-09-03 16:20:02 +0300

    Worked on frontend Posts controller; implemented all markup for views

--- SimpleBlog/App_Start/RouteConfig.cs
+++ SimpleBlog/App_Start/RouteConfig.cs
@@ -1,6 +1,6 @@
 ﻿using SimpleBlog.Controllers;
 using System;
-using System.Collections.Generic;
+using System.Collections.Generic; 
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -17,9 +17,15 @@ namespace SimpleBlog
 
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute("Login", "login", new { Controller = "Auth", action = "Login"}, namespaces);
+            routes.MapRoute("TagForRealThisTime", "tag/{idAndSlug}", new { controller = "Posts", action = "Tag" }, namespaces);
+            routes.MapRoute("Tag", "tag/{id}-{slug}", new { controller = "Posts", action = "Tag" }, namespaces);
 
-            routes.MapRoute("Logout", "logout", new { Controller = "Auth", action = "Logout" }, namespaces);
+            routes.MapRoute("PostForRealThisTime", "post/{idAndSlug}", new { controller = "Posts", action = "Show" }, namespaces);
+            routes.MapRoute("Post", "post/{id}-{slug}", new { controller = "Posts", action = "Show" }, namespaces);
+
+            routes.MapRoute("Login", "login", new { controller = "Auth", action = "Login"}, namespaces);
+
+            routes.MapRoute("Logout", "logout", new { controller = "Auth", action = "Logout" }, namespaces);
 
             routes.MapRoute("Home", "", new { controller = "Posts", action = "Index"}, namespaces);
                 
